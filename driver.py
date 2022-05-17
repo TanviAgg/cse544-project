1b48713fc7b1156f2141952a338b58bbfc3fcdd4
Tanvi Aggarwal <tanviaggarwal@Tanvis-MacBook-Air.local>
2022-05-17 12:11:36 -0400

    Refactor

--- driver.py
+++ driver.py
@@ -3,8 +3,9 @@ import pandas as pd
 from preprocessing import get_state_data, get_daily_cases_data, remove_outliers
 
 # States allocates: Connecticut (CT) and Florida (FL)
-if __name__ == "__main__":
-    # Mandatory Task 1: To clean the given dataset for cases
+
+
+def process_cases_data():
     covid_cases_data_path = './dataset/United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv'
 
     ct_state_cases_data, fl_state_cases_data = get_state_data(filename=covid_cases_data_path,
@@ -27,33 +28,45 @@ if __name__ == "__main__":
 
     ct_daily_cleaned_data = remove_outliers(ct_daily_cases_data)
     fl_daily_cleaned_data = remove_outliers(fl_daily_cases_data)
+    return ct_daily_cleaned_data, fl_daily_cleaned_data
 
-    # ct_daily_cleaned_data.to_csv('./processed/clean_ct_cases.csv')
-    # fl_daily_cleaned_data.to_csv('./processed/clean_fl_cases.csv')
 
-    # Mandatory Task 1: To clean the given dataset for vaccinations
+def process_vax_data():
     covid_vax_data_path = './dataset/COVID-19_Vaccinations_in_the_United_States_Jurisdiction.csv'
+
     ct_state_vax_data, fl_state_vax_data = get_state_data(filename=covid_vax_data_path,
-                                                              states=['CT', 'FL'],
-                                                              location_col_name='Location',
-                                                              cols=['Date', 'Location',
-                                                                    'Administered'])
+                                                          states=['CT', 'FL'],
+                                                          location_col_name='Location',
+                                                          cols=['Date', 'Location',
+                                                                'Administered'])
 
     ct_daily_vax_data = get_daily_cases_data(ct_state_vax_data,
-                                               location_col_name='Location',
-                                               date_col_name='Date',
-                                               non_cumulative_cols=[],
-                                               set_zero_for_negatives=True)
+                                             location_col_name='Location',
+                                             date_col_name='Date',
+                                             non_cumulative_cols=[],
+                                             set_zero_for_negatives=True)
     fl_daily_vax_data = get_daily_cases_data(fl_state_vax_data,
-                                               location_col_name='Location',
-                                               date_col_name='Date',
-                                               non_cumulative_cols=[],
-                                               set_zero_for_negatives=True)
+                                             location_col_name='Location',
+                                             date_col_name='Date',
+                                             non_cumulative_cols=[],
+                                             set_zero_for_negatives=True)
 
     ct_daily_cleaned_vax_data = remove_outliers(ct_daily_vax_data,
                                                 cols_to_consider=['Administered'])
     fl_daily_cleaned_vax_data = remove_outliers(fl_daily_vax_data,
                                                 cols_to_consider=['Administered'])
+    return ct_daily_cleaned_vax_data, fl_daily_cleaned_vax_data
+
+
+if __name__ == "__main__":
+    # Mandatory Task 1: To clean the given dataset for cases
+    ct_daily_cleaned_data, fl_daily_cleaned_data = process_cases_data()
+
+    ct_daily_cleaned_data.to_csv('./processed/clean_ct_cases.csv')
+    fl_daily_cleaned_data.to_csv('./processed/clean_fl_cases.csv')
+
+    # Mandatory Task 1: To clean the given dataset for vaccinations
+    ct_daily_cleaned_vax_data, fl_daily_cleaned_vax_data = process_vax_data()
 
     ct_daily_cleaned_vax_data.to_csv('./processed/clean_ct_vax.csv')
     fl_daily_cleaned_vax_data.to_csv('./processed/clean_fl_vax.csv')
