b0edc996f8bf109d99f4443324ae7e76e533af56
Tanvi Aggarwal <tanviaggarwal@Tanvis-MacBook-Air.local>
2022-05-16 23:29:57 -0400

    Select specific cols

--- driver.py
+++ driver.py
@@ -2,16 +2,15 @@ import pandas as pd
 
 from preprocessing import get_state_data, get_daily_data, remove_outliers
 
-
 # States allocates: Connecticut (CT) and Florida (FL)
 if __name__ == "__main__":
-
 	# Mandatory Task 1: To clean the given dataset
 	covid_cases_data_path = './dataset/United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv'
 
-	states_data = get_state_data(covid_cases_data_path, ['CT', 'FL'])
+	states_data = get_state_data(filename=covid_cases_data_path,
+								 states=['CT', 'FL'],
+								 cols=['submission_date', 'state', 'tot_cases', 'tot_death'])
 
 	daily_data = get_daily_data(states_data)
 
 	cleaned_data = remove_outliers(daily_data)
-
--- preprocessing.py
+++ preprocessing.py
@@ -2,13 +2,14 @@ from typing import Dict, Any, List
 import pandas as pd
 
 
-def get_state_data(filename: str, states: List[str], remove_nan: bool = True):
+def get_state_data(filename: str, states: List[str], cols: List[str], remove_nan: bool = True):
 	"""
 	Select data corresponding to each state
 	Additionally, remove missing (nan) values
 
 	:param filename: data path for full data
 	:param states: states for which we want data
+	:param cols: columns of interest
 	:param remove_nan: flag to check if nan values need to be removed
 
 	:return: List[dataframe]: data corresponding to each state
@@ -17,9 +18,13 @@ def get_state_data(filename: str, states: List[str], remove_nan: bool = True):
 	states_data = []
 	for state in states:
 		state_data = data_df.loc[data_df['state'] == state]
+		state_data_cols = state_data[cols]
+		total_rows = state_data_cols.shape[0]
 		if remove_nan:
-			state_data = state_data.dropna()
-		states_data.append(state_data)
+			state_data_cols = state_data_cols.dropna()
+			total_rows_after_nan_removal = state_data_cols.shape[0]
+			print("State: {} rows with missing values: {}".format(state, total_rows - total_rows_after_nan_removal))
+		states_data.append(state_data_cols)
 	return states_data
 
 
